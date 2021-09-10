2666cccdd50ade9cd518096a06fe4b8d816a9aca
Nick Petrie <nicholasjpetrie@gmail.com>
2021-09-10 10:05:41 -0500

    Add ability to delete an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,4 +35,11 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article #{params[:id]} was successfully deleted."
+    redirect_to articles_path
+  end
+
 end
