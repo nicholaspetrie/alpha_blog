f5eb1dd24da0b6033aef640d9ab46f68777b3d71
Nick <nicholasjpetrie@gmail.com>
2021-09-12 14:53:34 -0500

    Added session login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'signup', to: "users#new"
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
-  post 'login', to: 'session#create'
-  delete 'login', to: 'session#destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
