89e9d2068c299724919b3ed397b697147dc56038
Nick <nicholasjpetrie@gmail.com>
2021-09-12 17:00:01 -0500

    Add green/red text for flash messages based on type of notice or alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       flash[:notice] = "Logged in successfully"
       redirect_to user
     else
-      flash.new[:alert] = "There was something wrong with your login details"
+      flash.now[:alert] = "There was something wrong with your login details"
       render 'new'
     end
   end
